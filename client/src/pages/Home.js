1e79c8426ba5c427c003bfbf888ff9a6426d322a
Tastot Cedric <cedric_tastot@ens.univ-artois.fr>
2019-05-07 11:11:27 +0200

    Menu

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
+import logo from './logo.png';
+import logo2 from "./logo2.png";
 require("./Style.css");
 
 class Home extends Component {
     render() {
         return (
             <div className="home">
+                <img src={logo} className="logo"/>
                 <div className="ui center aligned raised very padded text container segment">
                     <h1 className="ui huge header">BIENVENUE</h1>
                     <a href="/quizz">
