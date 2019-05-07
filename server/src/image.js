dbfb8195ac182b8950a4f221741846935e9e818f
Masson Victor <victor_masson@ens.univ-artois.fr>
2019-05-07 15:23:42 +0200

    Upload et Download d'images implémenté

---
+++ server/src/image.js
@@ -0,0 +1,11 @@
+module.exports = class Image {
+    uploadImage(req,res) {
+        req.files.pictures.mv(__dirname+'/resources/img/'+req.files.picture.name,
+            (error) => {
+                if(error){
+                    return res.status(500).send(error);
+                }
+                res.json({ok:"ok"})
+            })
+    }
+};
