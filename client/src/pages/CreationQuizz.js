db32783f493d7de017f6f10b2cc10b238ec7650a
florian_chenot <chenotflo@gmail.com>
2019-05-07 17:18:11 +0200

    update

--- client/src/pages/CreationQuizz.js
+++ client/src/pages/CreationQuizz.js
@@ -6,16 +6,8 @@ class CreationQuizz extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            activeItem: '',
-            stock: {
-                "title": "",
-                "data": [
-                    {
-                        "question": "",
-                        "reponses": []
-                    }
-                ]
-            }
+            "title": "",
+            "data": []
         }
         ;
 
@@ -25,88 +17,126 @@ class CreationQuizz extends Component {
     }
 
     handleSubmit = (e) => {
-        alert('A name was submitted: ' + this.state.stock);
+        alert('A name was submitted: ' + this.state.data);
         e.preventDefault();
     };
 
     handleItemClick = (elem) => {
+        console.log(this.state);
         let doc = document.getElementsByClassName("active");
         doc[0].className = "item police";
         elem.target.className = "item active";
         console.log(elem.target);
-        if (elem.className)
-            this.setState({stock: this.state.stock.concat(this.state.activeItem)});
-        console.log(this.state.stock);
 
         if (elem.target.id !== 'g') {
-            if (document.getElementById("gen")) {
-                document.getElementById("gen").remove();
+            if (document.getElementById("gen").children[0]) {
+                document.getElementById("gen").children[0].remove();
+                document.getElementById("gen").children[0].remove();
             }
+            document.getElementById("description").setAttribute("value", this.state.stock.data[elem.target.id].question);
+            document.getElementById("reponse1").setAttribute("value", this.state.stock.data[elem.target.id].reponses[0]);
+            document.getElementById("reponse2").setAttribute("value", this.state.stock.data[elem.target.id].reponses[1]);
+            document.getElementById("reponse3").setAttribute("value", this.state.stock.data[elem.target.id].reponses[2]);
+            document.getElementById("reponse4").setAttribute("value", this.state.stock.data[elem.target.id].reponses[3]);
+        } else {
+            if (!document.getElementById("gen").children[0]) {
+                let node = "<p className=\"ui segment\">Quizz Name</p>\n" +
+                    "<div className=\"ui fluid icon input\">\n" +
+                    "<input id=\"QuizzName\" type=\"text\" placeholder=\"QuizzName\" onChange={this.handleChange}/>\n" +
+                    "</div>";
+                document.getElementById("gen").innerHTML += node;
+            }
+
         }
+
+
     };
 
     handleChange(e) {
-        this.setState({activeItem: e.target.value});
+        if (e.target.id==='g') {
+            let name = document.getElementById("QuizzName").value;
+            this.setState({title:name});
+        }
+        let description = document.getElementById("description").value;
+        let r1 = document.getElementById("reponse1").value;
+        let r2 = document.getElementById("reponse2").value;
+        let r3 = document.getElementById("reponse3").value;
+        let r4 = document.getElementById("reponse4").value;
+        let rs = [r1, r2, r3, r4];
+        let questions = {
+            description: description,
+            reponses: rs
+        };
+
+        this.setState({data:this.state.data.concat(questions)});
+        console.log(this.state);
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-
+            <form>
                 <div className="list">
                     <div className="ui grid">
                         <div className="four wide column">
                             <div className="ui vertical fluid tabular menu">
-                                <a id="g" className="item active" onClick={this.handleItemClick}>
+                                <p id="g" className="item active">
                                     General
-                                </a>
-                                <a id="1" className="police item" onClick={this.handleItemClick}>
+                                </p>
+                                <p id="0" className="police item">
+                                    0
+                                </p>
+                                <p id="1" className="item police">
                                     1
-                                </a>
-                                <a id="2" className="item police" onClick={this.handleItemClick}>
+                                </p>
+                                <p id="2" className="item police">
                                     2
-                                </a>
-                                <a id="3" className="item police" onClick={this.handleItemClick}>
-                                    3
+                                </p>
+                            </div>
+                            <div className="ui column">
+                                <a className="ui button"
+                                       onClick={this.handleChange}>
+                                    Next
                                 </a>
                             </div>
-                            <a id="+Q" className="ui button">
-                                + //modif
-                            </a>
+                            <div className="ui column">
+                                <input className="ui button" type="submit" value="Valider Quizz"
+                                       onClick={this.handleSubmit}/>
+                            </div>
                         </div>
                         <div className="eleven wide stretched column">
                             <div className="ui segment">
                                 <div id="gen" className="column">
                                     <p className="ui segment">Quizz Name</p>
                                     <div className="ui fluid icon input">
-                                        <input type="text" placeholder="QuizzName"/>
+                                        <input id="QuizzName" type="text" placeholder="QuizzName"
+                                        />
                                     </div>
                                 </div>
-                                <div className="column">
+                                <div className="column hidden">
                                     <p className="ui segment">Description de la question</p>
                                     <div className="ui fluid icon input">
-                                        <input type="text" placeholder="Description de la question"/>
+                                        <input id="description" type="text" placeholder="Description de la question"
+                                        />
                                     </div>
                                 </div>
-                                <div className="column">
+                                <div className="column hidden">
                                     <p className="ui segment">Reponses</p>
                                     <div className="ui three column double grid">
                                         <div className="ui input column">
-                                            <input type="text" placeholder="Reponse 1 modif image"/>
+                                            <input id="reponse1" type="text" placeholder="Reponse 1 modif image"
+                                            />
                                         </div>
                                         <div className="ui input column">
-                                            <input type="text" placeholder="Reponse 2"/>
+                                            <input id="reponse2" type="text" placeholder="Reponse 2"
+                                            />
                                         </div>
                                         <div className="ui input column">
-                                            <input type="text" placeholder="Reponse 3"/>
+                                            <input id="reponse3" type="text" placeholder="Reponse 3"
+                                            />
                                         </div>
                                         <div className="ui input column">
-                                            <input type="text" placeholder="Reponse 4"/>
-                                        </div>
-                                        <div className="ui column">
-                                            <a id="+R" className="ui button">
-                                                Valider Quizz Complet //modif
-                                            </a>
+                                            <input id="reponse4" type="text" placeholder="Reponse 4"
+                                            />
                                         </div>
                                     </div>
                                 </div>
@@ -114,22 +144,6 @@ class CreationQuizz extends Component {
                         </div>
                     </div>
                 </div>
-                <label>
-                    <h1
-                        id="11"
-                        className="">
-                        0
-                    </h1>
-                    <h1 id="2" className="hidden">
-                        coucou
-                    </h1>
-                    <input
-                        type="text"
-                        value={this.state.activeItem}
-                        onChange={this.handleChange}
-                    />
-                </label>
-                <input type="submit" value="Next" onClick={this.handleItemClick}/>
             </form>
         );
     }
